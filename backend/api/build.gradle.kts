51711c78852311950b4245306516d3f89db97243
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2024-05-16 23:12:40 +0200

    api: fixed using java21 instead of 17 for the pipeline

--- backend/api/build.gradle.kts
+++ backend/api/build.gradle.kts
@@ -8,7 +8,7 @@ group = "dev.dubsky.routeguide"
 version = "0.0.1"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 configurations {
