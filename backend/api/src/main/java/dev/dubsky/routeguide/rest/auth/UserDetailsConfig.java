bd9cb59de9dea272de5eaf0579f5437ce9c05f23
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2024-05-17 03:56:11 +0200

    api: added temporary UserDetailsConfig RG-5

---
+++ backend/api/src/main/java/dev/dubsky/routeguide/rest/auth/UserDetailsConfig.java
@@ -0,0 +1,23 @@
+package dev.dubsky.routeguide.rest.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class UserDetailsConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user")
+                        .password("{noop}password")
+                        .roles("USER")
+                        .build()
+        );
+    }
+}
+
