6a0074c4f9eb93ae01c19b3d3581798869507b8a
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2024-05-21 14:43:39 +0200

    api: applied DTO to userController

--- backend/api/src/main/java/dev/dubsky/routeguide/rest/controller/UserController.java
+++ backend/api/src/main/java/dev/dubsky/routeguide/rest/controller/UserController.java
@@ -1,5 +1,6 @@
 package dev.dubsky.routeguide.rest.controller;
 
+import dev.dubsky.routeguide.rest.dto.UserDTO;
 import dev.dubsky.routeguide.rest.model.User;
 import dev.dubsky.routeguide.rest.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
@@ -17,26 +19,26 @@ public class UserController {
 
     @GetMapping("/{id}")
     @PreAuthorize("hasRole('USER')")
-    public User getUserById(@PathVariable Long id) {
-        return userService.findById(id);
+    public UserDTO getUserById(@PathVariable Long id) {
+        return new UserDTO(userService.findById(id));
     }
 
     @GetMapping
     @PreAuthorize("hasRole('USER')")
-    public List<User> getAllUsers() {
-        return userService.findAll();
+    public List<UserDTO> getAllUsers() {
+        return userService.findAll().stream().map(UserDTO::new).collect(Collectors.toList());
     }
 
     @PostMapping
-    public User createUser(@RequestBody User user) {
-        return userService.save(user);
+    public UserDTO createUser(@RequestBody User user) {
+        return new UserDTO(userService.save(user));
     }
 
     @PutMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
-    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+    public UserDTO updateUser(@PathVariable Long id, @RequestBody User user) {
         user.setId(id);
-        return userService.save(user);
+        return new UserDTO(userService.save(user));
     }
 
     @DeleteMapping("/{id}")
