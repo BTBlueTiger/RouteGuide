7fad4ca15134b291793b4429ec46c32bcd3a7c95
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2024-05-25 19:48:03 +0200

    api: fixed envReader

--- backend/api/src/main/java/dev/dubsky/routeguide/rest/config/EnvReader.java
+++ backend/api/src/main/java/dev/dubsky/routeguide/rest/config/EnvReader.java
@@ -1,17 +1,16 @@
 package dev.dubsky.routeguide.rest.config;
 
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Getter
+@Component
 public class EnvReader {
 
     @Value("${GOOGLE_API_KEY}")
     private String googleApiKey;
 
-    @Bean
-    public String googleApiKey() {
-        return googleApiKey;
-    }
 }
