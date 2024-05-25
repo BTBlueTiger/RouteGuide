85dac75d9817fa7abb03e6be9ec4df089d0e5843
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2024-05-25 15:58:11 +0200

    database: updated creation script for tables

--- backend/database/create_tables.sql
+++ backend/database/create_tables.sql
@@ -1,59 +1,57 @@
-create table "user"
-(
-    user_id  integer   not null
-        constraint user_pk
-            primary key,
-    username varchar   not null,
-    email    varchar   not null,
-    password varchar   not null,
-    creation timestamp not null
-);
-
-alter table "user"
-    owner to admin;
-
 create table "group"
 (
-    group_id integer not null
+    group_id serial
         constraint group_pk
             primary key,
-    status   varchar,
-    location varchar
+    name     varchar not null
 );
 
 alter table "group"
     owner to admin;
 
-create table route
+create table companies
 (
-    route_id   integer not null
-        constraint route_pk
+    company_id  serial
+        constraint companies_pk
             primary key,
-    start      varchar,
-    "end"      varchar,
-    included   varchar,
-    creator_id integer
-        constraint route_user_user_id_fk
-            references "user",
-    group_id   integer
-        constraint route_group_group_id_fk
-            references "group"
+    name        varchar not null
+        constraint companies_pk_2
+            unique,
+    mail_ending varchar
 );
 
-alter table route
+alter table companies
+    owner to admin;
+
+create table "user"
+(
+    user_id    serial
+        constraint user_pk
+            primary key,
+    email      varchar                                   not null,
+    username   varchar                                   not null,
+    password   varchar,
+    role       varchar default 'USER'::character varying not null,
+    creation   date                                      not null,
+    company_id integer
+        constraint company_fk
+            references companies
+);
+
+alter table "user"
     owner to admin;
 
 create table user_group
 (
+    user_id  integer not null
+        constraint user_group_user_user_id_fk
+            references "user",
     group_id integer not null
         constraint user_group_group_group_id_fk
             references "group",
-    user_id  integer not null
-        constraint user_group_user_user_id_fk
-            references "user"
+    constraint user_group_pk
+        primary key (user_id, group_id)
 );
 
 alter table user_group
     owner to admin;
-
-
