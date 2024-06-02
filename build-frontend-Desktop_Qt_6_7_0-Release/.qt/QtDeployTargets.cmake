f6e9af5d2ecc21cbec79bc4d4da4d56ee7912889
Malte Kanders <kamalte@web.de>
2024-06-02 21:50:25 +0200

    Bug build folder not in gitignore

--- build-frontend-Desktop_Qt_6_7_0-Release/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_tst_unit_FILE /home/kamalte/Documents/RouteGuide/build-frontend-Desktop_Qt_6_7_0-Release/tests/unit/tst_unit)
-set(__QT_DEPLOY_TARGET_tst_integration_FILE /home/kamalte/Documents/RouteGuide/build-frontend-Desktop_Qt_6_7_0-Release/tests/integration/tst_integration)
-set(__QT_DEPLOY_TARGET_frontend_FILE /home/kamalte/Documents/RouteGuide/build-frontend-Desktop_Qt_6_7_0-Release/frontend)
--- build-frontend-Desktop_Qt_6_7_0-Release/tests/unit/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kamalte/Documents/RouteGuide/frontend/tests/unit
-# Build directory: /home/kamalte/Documents/RouteGuide/build-frontend-Desktop_Qt_6_7_0-Release/tests/unit
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(tst_validationTextfield "tst_validationTextfield")
-set_tests_properties(tst_validationTextfield PROPERTIES  _BACKTRACE_TRIPLES "/home/kamalte/Documents/RouteGuide/frontend/tests/unit/CMakeLists.txt;36;add_test;/home/kamalte/Documents/RouteGuide/frontend/tests/unit/CMakeLists.txt;0;")
