7ef021504aebe5343869095e8ff725acfa43c36f
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2024-07-03 14:34:34 +0200

    api: added doc to CLog

--- backend/api/src/main/java/dev/dubsky/routeguide/rest/utility/CLog.java
+++ backend/api/src/main/java/dev/dubsky/routeguide/rest/utility/CLog.java
@@ -1,11 +1,19 @@
 package dev.dubsky.routeguide.rest.utility;
 
+/**
+ * Class for colored console output
+ */
 public class CLog {
     private static final String RESET = "\033[0m";
     private static final String GREEN = "\033[92m";
     private static final String YELLOW = "\033[93m";
     private static final String RED = "\033[91m";
     
+    /**
+     * Print text with color
+     * @param colorCode 0 - green, 1 - yellow, 2 - red, other - reset
+     * @param text text to print
+     */
     public static void out(int colorCode, String text) {
         String color;
         switch (colorCode) {
