3d394784e1f24ad19c219f748dc453d73d12fc1e
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2024-07-09 02:14:07 +0200

    api: updated AdvancedLog dependency to v0.1.3

--- backend/api/build.gradle.kts
+++ backend/api/build.gradle.kts
@@ -66,7 +66,7 @@ dependencies {
     implementation("org.slf4j:slf4j-simple:1.7.25")
 
     // AdvancedLog
-    implementation("dev.dubsky:advancedlog:0.1.2")
+    implementation("dev.dubsky:advancedlog:0.1.3")
 }
 
 tasks.withType<Test> {
